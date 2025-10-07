25a9233a95436157bc52c08fb4f3e4c263c639b1
Javlonbek <javlonrahmatullayev07@gmail.com>
2025-10-07 08:33:38 +0500

    add some changes

--- resources/views/onlyoffice-editor.blade.php
+++ resources/views/onlyoffice-editor.blade.php
@@ -1,18 +1,15 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <head>
     <meta charset="utf-8">
-    <title>OnlyOffice Editor</title>
+    <title>OnlyOffice</title>
     <script src="{{ $documentServerUrl }}/web-apps/apps/api/documents/api.js"></script>
 </head>
 <body style="margin:0">
 <div id="placeholder" style="width:100%; height:100vh;"></div>
-
 <script>
-    const config = {!! json_encode($config, JSON_UNESCAPED_SLASHES) !!};
-    config.token = "{{ $token }}"; // JWT shu yerda
-
-    new DocsAPI.DocEditor("placeholder", config);
+    const cfg = {!! json_encode($config, JSON_UNESCAPED_SLASHES) !!};
+    new DocsAPI.DocEditor("placeholder", cfg);
 </script>
 </body>
 </html>
